83fbeeab1bd90197f488cdd1c885c1fe1cc762a0
isabelajs <isabelajimenezs96@gmail.com>
2025-12-01 21:09:27 -0500

    Refactor App component to use Layout for both main and character detail routes, simplifying structure and enhancing consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './Layouts/Layout'
-import CharactersPage from './pages/CharactersPage'
-import CharacterDetailPage from './pages/CharacterDetailPage'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<CharactersPage />} />
-        <Route path="/character/:id" element={<CharacterDetailPage />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route path="/character/:id" element={<Layout />} />
+    </Routes>
   )
 }
 
